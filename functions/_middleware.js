b2fe1e61314987f6f7e2db4390812854c299e884
syuto koike <syutokoike@syutonoMacBook-Pro.local>
2026-07-30 23:51:06 +0900

    test pages function

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,30 +1,3 @@
-export async function onRequest(context) {
-
-  const username = "k_syuto_portfolio";
-  const password = "0922password";
-
-  const auth = context.request.headers.get("Authorization");
-
-  if (!auth) {
-    return new Response("Authentication required", {
-      status: 401,
-      headers: {
-        "WWW-Authenticate": 'Basic realm="Portfolio"',
-      },
-    });
-  }
-
-  const base64 = auth.split(" ")[1];
-  const decoded = atob(base64);
-
-  if (decoded !== `${username}:${password}`) {
-    return new Response("Unauthorized", {
-      status: 401,
-      headers: {
-        "WWW-Authenticate": 'Basic realm="Portfolio"',
-      },
-    });
-  }
-
-  return context.next();
+export async function onRequest() {
+  return new Response("Functions is working");
 }
