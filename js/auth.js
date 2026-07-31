f5b61c33ba11656dd34c2c000f8e5aa4fc72ca13
syu <koikesuitors@gmail.com>
2026-07-31 13:24:33 +0900

    Update password expiration

---
+++ js/auth.js
@@ -0,0 +1,25 @@
+function checkPassword() {
+  const password = document.getElementById("password").value;
+
+  const correctPassword = "syu-web2026";
+
+  if (password === correctPassword) {
+
+    // 24時間後の有効期限を保存
+    const expire =
+      Date.now() + (24 * 60 * 60 * 1000);
+
+    localStorage.setItem(
+      "authExpire",
+      expire
+    );
+
+    window.location.href = "index.html";
+
+  } else {
+
+    document.getElementById("error").textContent =
+      "パスワードが違います";
+
+  }
+}
